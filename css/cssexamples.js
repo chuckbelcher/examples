06628e652ec6784695dfdadd2cf5574f595b12fc
Chuck Belcher <cfbelcher@pateogroup.com>
2021-02-03 21:46:17 -0500

    added js function to rotate through border-radious configurations based on radio button

--- css/cssexamples.js
+++ css/cssexamples.js
@@ -1,3 +1,12 @@
-BRImage=document.querySelector("#border-radius");
-BRImage.classList="round"
-console.log(BRImage);
+brImage = document.querySelector("#border-radius");
+brImage.classList="round";
+brRadio = document.querySelectorAll('input[name="class"]');
+
+const changeRadioOption = () => {
+    for(var i = 0; i < brRadio.length; i++) {
+        if(brRadio[i].checked)
+            brImage.classList = brRadio[i].value;
+      }
+}
+
+brRadio.forEach(brRadio => brRadio.addEventListener('click', changeRadioOption))
